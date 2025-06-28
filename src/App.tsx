9cf641ba01cf69490a85095fed48ef4dc088ce88
[dyad] <ngviendong@gmail.com>
2025-06-28 13:37:30 +0700

    [dyad] Đã xóa các tham chiếu đến Shadcn Toaster cũ trong App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster as ShadcnToaster } from "@/components/ui/toaster"
+// import { Toaster as ShadcnToaster } from "@/components/ui/toaster" // Removed
 import { Toaster } from "@/components/ui/sonner"
 
 import Layout from './components/Layout';
@@ -55,7 +55,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-      <ShadcnToaster />
+      {/* <ShadcnToaster /> */} {/* Removed */}
       <Toaster richColors />
     </QueryClientProvider>
   );

1e65c22ac226a1bac5b5825e604ebe72a549068f
[dyad] <ngviendong@gmail.com>
2025-06-28 13:55:20 +0700

    [dyad] Đã bọc ứng dụng bằng SecureAuthProvider trong main.tsx. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { SecureAuthProvider } from './hooks/useSecureAuth.tsx'; // Import SecureAuthProvider
+import React from 'react'; // Import React for StrictMode
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SecureAuthProvider> {/* Wrap App with SecureAuthProvider */}
+      <App />
+    </SecureAuthProvider>
+  </React.StrictMode>
+);
